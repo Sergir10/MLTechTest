7224f20ab634287d1c1097837e02f5bb36262742
Sergio Giraldo <SGiraldo@condorlabs.io>
2021-01-23 08:00:43 -0500

    Add Pod and Git

---
+++ MLTechTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MLTechTest
+//
+//  Created by Sergio Giraldo on 23/01/21.
+//  Copyright © 2021 Cebollitas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
