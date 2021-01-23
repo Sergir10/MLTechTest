9a61843538191d9838bbd7875016ebb7339ba28b
Sergio Giraldo <SGiraldo@condorlabs.io>
2021-01-23 08:04:52 -0500

    Remove storyboards and Add MLTechCore module

---
+++ MLTechCore/MLTechCore/MLTechCore.h
@@ -0,0 +1,19 @@
+//
+//  MLTechCore.h
+//  MLTechCore
+//
+//  Created by Sergio Giraldo on 23/01/21.
+//  Copyright © 2021 Cebollitas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MLTechCore.
+FOUNDATION_EXPORT double MLTechCoreVersionNumber;
+
+//! Project version string for MLTechCore.
+FOUNDATION_EXPORT const unsigned char MLTechCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MLTechCore/PublicHeader.h>
+
+
