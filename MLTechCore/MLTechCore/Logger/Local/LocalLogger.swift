22e9500ede8cd9c766b059c261c60133fa84e68b
Sergio Giraldo <SGiraldo@condorlabs.io>
2021-01-23 20:09:42 -0500

    Create LogModel for LocalLogger

--- MLTechCore/MLTechCore/Logger/Local/LocalLogger.swift
+++ MLTechCore/MLTechCore/Logger/Local/LocalLogger.swift
@@ -1,5 +1,5 @@
 //
-//  Logger.swift
+//  LocalLogger.swift
 //  MLTechCore
 //
 //  Created by Sergio Giraldo on 23/01/21.
@@ -8,42 +8,6 @@
 
 import Foundation
 import os
-
-public enum LogLevel: String {
-    case debug = "[🐞 Debug]"
-    case info = "[ℹ️ Information]"
-    case error = "[⛔️ Error]"
-    case fault = "[⚠️ Fault]"
-    case notice = "[📌 Notice]"
-
-    var value: OSLogType {
-        switch self {
-        case .debug: return .debug
-        case .info: return .info
-        case .error: return .error
-        case .fault: return .fault
-        case .notice: return .default
-        }
-    }
-}
-
-public struct LogModel {
-    var level: LogLevel
-    var data: Any?
-    var message: String = ""
-    var file: String = #file
-    var line: Int = #line
-    var function: String = #function
-
-    public init(level: LogLevel, data: Any?, message: String = "", file: String = #file, line: Int = #line, function: String = #function) {
-        self.level = level
-        self.data = data
-        self.message = message
-        self.file = file
-        self.line = line
-        self.function = function
-    }
-}
 
 public struct LocalLogger {
     public static func register(model: LogModel) {
